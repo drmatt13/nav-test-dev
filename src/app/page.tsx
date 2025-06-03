094dcae07ce04e5f323650d10735224e5e92b4af
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2025-06-03 17:01:18 -0400

    6/3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,61 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [size, setSize] = useState(6);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <>
+      {/* Sticky Navbar */}
+      <nav
+        className="w-full bg-black/80 backdrop-blur sticky top-0 z-50 px-4 flex justify-between items-center"
+        style={{ height: `${size}rem` }}
+      >
+        <div className="text-white">Navbar</div>
+        <div className="flex text-2xl gap-4">
+          <button
+            onClick={() => setSize(size - 1)}
+            className="h-10 aspect-square bg-white/90 hover:bg-white cursor-pointer transition-colors flex justify-center items-center rounded"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            -
+          </button>
+          <button
+            onClick={() => setSize(size + 1)}
+            className="h-10 aspect-square bg-white/90 hover:bg-white cursor-pointer transition-colors flex justify-center items-center rounded"
           >
-            Read our docs
-          </a>
+            +
+          </button>
+        </div>
+      </nav>
+
+      <main className="w-full flex flex-col text-lg font-bold">
+        <div
+          className="w-full flex justify-center items-center bg-blue-400"
+          style={{ height: `calc(100svh - ${size}rem)` }}
+        >
+          Map
+        </div>
+
+        <div className="w-full flex p-12 justify-center items-center">
+          Component 1
+        </div>
+        <div className="w-full flex p-12 justify-center items-center">
+          Component 2
+        </div>
+        <div className="w-full flex p-12 justify-center items-center">
+          Component 3
+        </div>
+        <div className="w-full flex p-12 justify-center items-center">
+          Component 4
+        </div>
+        <div className="w-full flex p-12 justify-center items-center">
+          Component 5
+        </div>
+        <div className="w-full flex p-12 justify-center items-center">
+          Component 6
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
